02086455ed658a77bf4448d3691d3376cb07cde4
Kuzaikun <arien02.contacto@gmail.com>
2019-02-11 11:11:19 -0600

    Initial commit of proyect Personnal web

---
+++ resources/views/html-config/head.blade.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<meta name="description" content="">
+
+
+<title>Amazing Ari Web</title>
+
+<!-- load bootstrap from a cdn -->
+<link rel="stylesheet" href={{asset("css/app.css")}}>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  @include('html-config.head')
+</head>
+
+<body>
+  <div class="container">
+    <header class="row">
+      @include('layouts.header')
+    </header>
+    <footer class="row">
+      @include('layouts.footer')
+    </footer>
+  </div>
+</body>
+
+</html>
