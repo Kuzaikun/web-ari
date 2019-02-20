8559a90d9acda411d75055bb64a394ccdd8b6ce2
Kuzaikun <arien02.contacto@gmail.com>
2019-02-19 18:20:03 -0600

    Changes of CSS :)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,8 +8,11 @@
   <div class="container">
     <header>
       @include('layouts.header')
+      <img class="home-image" src="{{URL::asset('images/home-image.jpg')}}">
     </header>
-  
+    <div class="home-image">
+
+    </div>
     <footer>
       @include('layouts.footer')
     </footer>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,4 +1,5 @@
 <div class="scrollmenu">
+  <a href="/"><img class="img-isotype" src="{{URL::asset('images/isotipo1.png')}}"></a>
   <a href="/">Home</a>
   <a href="/about">News</a>
   <a href="/projects">Contact</a>
