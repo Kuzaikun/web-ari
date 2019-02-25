901121e8659cf09c75bc5bfe0d55c5b44cc8c516
unknown <arien02.contacto@gmail.com>
2019-02-24 18:51:54 -0600

    Changes of style :)

--- resources/views/html-config/head.blade.php
+++ resources/views/html-config/head.blade.php
@@ -1,8 +1,5 @@
-<meta charset="utf-8">
-<meta name="description" content="">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-<title>Amazing Ari Web</title>
-
-<!-- Load Styles-->
-<link href="{{ URL::asset('css/weaboostyle.css') }}" rel="stylesheet" type="text/css" >
+<title>Kuzai web</title>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+<link href="{{URL::asset('css/main.css')}}" rel="stylesheet" type="text/css" >
+<noscript><link href="{{URL::asset('css/noscript.css')}}" rel="stylesheet" type="text/css" ></noscript>
---
+++ resources/views/html-config/scripts.blade.php
@@ -0,0 +1,7 @@
+<script src="{{ URL::asset('js/jquery.min.js') }}" type="text/javascript" ></script>
+<script src="{{ URL::asset('js/jquery.scrollex.min.js') }}" type="text/javascript" ></script>
+<script src="{{ URL::asset('js/jquery.scrolly.min.js') }}" type="text/javascript" ></script>
+<script src="{{ URL::asset('js/browser.min.js') }}" type="text/javascript" ></script>
+<script src="{{ URL::asset('js/breakpoints.min.js') }}" type="text/javascript" ></script>
+<script src="{{ URL::asset('js/util.js') }}" type="text/javascript" ></script>
+<script src="{{ URL::asset('js/main.js') }}" type="text/javascript" ></script>
